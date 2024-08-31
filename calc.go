3b9ae580eacc11c3d9989992ea7e5bd014222020
segunf <olusegun.fagbamiye@gmail.com>
2024-08-31 15:17:32 -0400

    adding documentation for package

--- calc.go
+++ calc.go
@@ -1,5 +1,8 @@
+// A package that performs addition of two integers
 package calc
 
+// Add is a function that takes two integer values and returns their sum
+// It follows the rules specified by https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
